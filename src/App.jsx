643f005ddde5c4b53bd54ec36bc16fd5315ffa8d
DHRUV VERMA <azaditya129@gmail.com>
2025-12-10 00:42:21 +0000

    initial project setup with application and component structure

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Dhruv Verma",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "azaditya129@gmail.com",
+  gitHub: "student-og",
+  instagram: "itsmedhruvverma",
+  linkedIn: "codewithai",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
